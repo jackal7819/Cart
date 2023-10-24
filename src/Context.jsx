111991f24612a2cfaa77f53b344e4b2e8fbd55eb
Viktor Filippov <jackal3166@gmail.com>
2023-10-24 23:44:37 +0300

    Add remove

--- src/Context.jsx
+++ src/Context.jsx
@@ -22,9 +22,11 @@ export const AppProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(reducer, initialState);
 
 	const clearCart = () => dispatch({ type: CLEAR_CART });
+    
+    const remove = (id) => dispatch({ type: REMOVE, payload: { id } });
 
 	return (
-		<AppContext.Provider value={{ ...state, clearCart }}>
+		<AppContext.Provider value={{ ...state, clearCart, remove }}>
 			{children}
 		</AppContext.Provider>
 	);
